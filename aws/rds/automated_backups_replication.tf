4fa1b301974cd9317a338bfc6dac2bad73edf82e
Akhil Anil <akhil.anil@funderpro.com>
2026-01-23 11:10:16 +0100

    chore(rds): Update source db instance arn and count var

--- aws/rds/automated_backups_replication.tf
+++ aws/rds/automated_backups_replication.tf
@@ -1,9 +1,9 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_db_instance_automated_backups_replication" "this" {
-  count = var.create && var.enable_backup_replication ? 1 : 0
+  count = var.enable_backup_replication ? 1 : 0
 
-  source_db_instance_arn = aws_db_instance.this[0].arn
+  source_db_instance_arn = aws_db_instance.this.arn
   kms_key_id             = "arn:aws:kms:${var.backup_replication_region}:${data.aws_caller_identity.current.account_id}:alias/aws/rds"
   region                 = var.backup_replication_region
   retention_period       = var.backup_replication_retention_days
