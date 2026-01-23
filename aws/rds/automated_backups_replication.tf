652df2db8a1dfcce7b603419d2544de5f717685f
Akhil Anil <akhil.anil@funderpro.com>
2026-01-23 12:56:54 +0100

    chore(rds): Updated replica region provider

--- aws/rds/automated_backups_replication.tf
+++ aws/rds/automated_backups_replication.tf
@@ -3,9 +3,10 @@ data "aws_caller_identity" "current" {}
 resource "aws_db_instance_automated_backups_replication" "this" {
   count = var.enable_backup_replication ? 1 : 0
 
+  provider = aws.replica_region
+
   source_db_instance_arn = aws_db_instance.this.arn
   kms_key_id             = "arn:aws:kms:${var.backup_replication_region}:${data.aws_caller_identity.current.account_id}:alias/aws/rds"
-  region                 = var.backup_replication_region
   retention_period       = var.backup_replication_retention_days
 
   # Only needed for MySQL/MariaDB engines
