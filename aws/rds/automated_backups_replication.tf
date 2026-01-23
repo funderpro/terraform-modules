04a0442ae593089ced5fa69c82814558e51c8786
Akhil Anil <akhil.anil@funderpro.com>
2026-01-23 13:26:49 +0100

    chore(rds): Updated provider region name

--- aws/rds/automated_backups_replication.tf
+++ aws/rds/automated_backups_replication.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_db_instance_automated_backups_replication" "this" {
   count = var.enable_backup_replication ? 1 : 0
 
-  provider = aws.replica_region
+  provider = aws.backup_region
 
   source_db_instance_arn = aws_db_instance.this.arn
   kms_key_id             = "arn:aws:kms:${var.backup_replication_region}:${data.aws_caller_identity.current.account_id}:alias/aws/rds"
